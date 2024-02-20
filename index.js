f74f04e95e603e75e885bc1883b34d177d928416
Idris110 <ratlamwalaidris110@gmail.com>
2024-02-20 18:41:22 +0530

    fetch amazon for ratings

--- index.js
+++ index.js
@@ -12,6 +12,46 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+async function fetchAmazon(url){
+    console.log("Fetching amazon .....")
+    
+    let response = await axios.request({
+        method: "GET",
+        url,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        }
+    }).catch((err) => console.log(err));
+
+    if (response === undefined) {
+        console.log("Amazon response undfined");
+        return null
+    }
+    if (response.status !== 200) {
+        console.log("Error occurred while fetching data from amazon");
+        return;
+    }
+
+    const $ = cheerio.load(response.data);
+    let amzn = {};
+    
+    amzn.price = $('#corePriceDisplay_desktop_feature_div').find('.a-price-whole').text();
+    amzn.url = url; 
+
+    rate = {};
+    ratings = $('#histogramTable').text();
+    ratings = ratings.split('r');
+    rate.one = ratings[5];
+    rate.two = ratings[4].slice(0, -5);
+    rate.three = ratings[3].slice(0, -5);
+    rate.four = ratings[2].slice(0, -5);
+    rate.five = ratings[1].slice(0, -5);
+    // console.log(rate);
+    amzn.rating = rate;
+
+    return amzn;
+}
+
 async function fetchData(url) {
     const finalTitle = url.split("/")[5];
     console.log("Prod id :", finalTitle);
@@ -45,8 +85,11 @@ async function fetchData(url) {
     }).toArray()[1];
     data.orgRat = $('fieldset').find('.orig-rating-dim').text();
     data.adjRat = $('fieldset').find('#adjusted-rating-large').text();
+    data.fakeReviews = (100 - parseInt(data.adjRev.replace(/\,/g, ''))*100 / parseInt(data.totRev.replace(/\,/g, ''))).toFixed(2) + "%";
     data.img = $('#product_image').find('img').attr('src');
-
+    
+    
+    data.amazon = await fetchAmazon(url); //fetch amazon
 
     let trusted = {};
     trusted.title = "Most Trusted Reviews";
@@ -125,8 +168,8 @@ async function fetchData(url) {
 
     let del = {}
     wc.title = "Deleted Reviews";
-    wc.perc = $('#details-dl').find(".pct-percent").text();
-    data.del = wc;
+    wc.num = $('#details-dl').find(".pct-percent").text();
+    data.del = del;
 
     return data;
 }
@@ -141,6 +184,16 @@ async function fetchData(url) {
 app.get('/', function (req, res) {
 
     res.sendFile(path.join(__dirname, '/search.html'));
+});
+
+app.get('/amzn', function (req, res) {
+    fetchAmazon(
+        "https://www.amazon.in/Samsung-25W-Travel-Adapter/dp/B08VFF6JQ8/ref=pd_sbs_d_sccl_5_3/260-7665289-0966040?pd_rd_w=PjqJ2&content-id=amzn1.sym.a9e12e68-4e49-43d0-a6b4-fd1619ccac52&pf_rd_p=a9e12e68-4e49-43d0-a6b4-fd1619ccac52&pf_rd_r=QFF7RDAKYX16GNADKMT4&pd_rd_wg=PyTaT&pd_rd_r=fe91aaa4-5f23-4abc-9698-f7c33fad539b&pd_rd_i=B08VFF6JQ8&psc=1"
+    ).then((data) => {
+        res.send(data);
+        // fs.writeFileSync('scrappedMobile.html', html);
+        // res.send("hello");
+    })
 });
 
 app.get('/testing', function (req, res) {
