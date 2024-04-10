764dcc5ee47f5053e70bcc61e334261240949716
Idris110 <ratlamwalaidris110@gmail.com>
2024-04-10 23:21:04 +0530

    Add fetch news feature

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 var bodyParser = require('body-parser');
-// var cors = require("cors");
+
 
 const port = process.env.PORT || 8080;
 const app = express();
-// const corsOpts = {
-//     origin: '*',
-
-//     methods: [
-//       'GET',
-//       'POST',
-//     ],
-
-//     allowedHeaders: [
-//       'Content-Type',
-//     ],
-//   };
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -71,166 +59,186 @@ async function fetchAmazon(url) {
     return amzn;
 }
 
+
+
 async function fetchData(url) {
-    const finalTitle = url.split("/")[5];
-    console.log("Prod id :", finalTitle);
-    let response = await axios.request({
-        method: "GET",
-        url: `https://reviewmeta.com/amazon-in/${finalTitle}`,
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    try {
+        const finalTitle = url.split("/")[5];
+        console.log("Prod id :", finalTitle);
+        let response = await axios.request({
+            method: "GET",
+            url: `https://reviewmeta.com/amazon-in/${finalTitle}`,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+            }
+        }).catch((err) => console.log(""));
+
+        if (response === undefined) {
+            console.log("Fakespot response undfined");
+            return null
         }
-    }).catch((err) => console.log(""));
+        if (response.status !== 200) {
+            console.log("Error occurred while fetching data");
+            return;
+        }
 
-    if (response === undefined) {
-        console.log("Fakespot response undfined");
-        return null
+        let $ = cheerio.load(response.data);
+
+        let data = {};
+
+        data.prodName = $('#product_name').find('span').text().replace(/\t/g, '').replace(/\n/g, '');
+        data.totRev = $('fieldset').find('b').map(function () {
+            return $(this).text().trim();
+        }).toArray()[0];
+        data.adjRev = $('fieldset').find('b').map(function () {
+            return $(this).text().trim();
+        }).toArray()[1];
+        data.orgRat = $('fieldset').find('.orig-rating-dim').text();
+        data.adjRat = $('fieldset').find('#adjusted-rating-large').text();
+        data.fakeReviews = (100 - parseInt(data.adjRev.replace(/\,/g, '')) * 100 / parseInt(data.totRev.replace(/\,/g, ''))).toFixed(2) + "%";
+        data.img = $('#product_image').find('img').attr('src');
+
+
+        data.amazon = await fetchAmazon(url); //fetch amazon
+
+        let trusted = {};
+
+        trusted.rating = $('#good-reviews').find("#sample_reviews").find(".bw-rating").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        // .first().text().replace(/\t/g, '').replace(/\n/g, '');
+        trusted.review = $('#good-reviews').find(".show-actual-review").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
+        }).toArray();
+        // .first().text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
+
+        trusted.showMore = $('#good-reviews').find(".show-actual-review").find("a").map(function () {
+            return $(this).attr('href');
+        }).toArray();
+
+        let mostTrusted = {};
+        mostTrusted.title = "Most Trusted Reviews";
+        let reviews = [];
+        for (let i = 0; i < 5; i++) {
+            reviews.push({
+                rating: trusted.rating[i],
+                review: trusted.review[i],
+                showMore: trusted.showMore[i],
+            })
+        }
+        mostTrusted.reviews = reviews;
+        data.trusted = mostTrusted;
+
+        let ltrusted = {};
+        ltrusted.rating = $('#bad-reviews').find("#sample_reviews").find(".bw-rating").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        ltrusted.review = $('#bad-reviews').find(".show-actual-review").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
+        }).toArray();
+        ltrusted.showMore = $('#bad-reviews').find(".show-actual-review").find("a").map(function () {
+            return $(this).attr('href');
+        }).toArray();
+
+        let leastTrusted = {};
+        leastTrusted.title = "Least Trusted Reviews";
+        let lreviews = [];
+        for (let i = 0; i < 5; i++) {
+            lreviews.push({
+                rating: ltrusted.rating[i],
+                review: ltrusted.review[i],
+                showMore: ltrusted.showMore[i],
+            })
+        }
+        leastTrusted.reviews = lreviews;
+        data.leastTrusted = leastTrusted;
+
+
+        let unvPur = {}
+        unvPur.title = "Unverified Purchases";
+        unvPur.perc = $('#details-vp').find(".pct-percent").text();
+        unvPur.num = $('#details-vp').find(".col-md-6").find('b').first().text();
+        // unvPur.num = parseInt(data.totRev.replace(/\,/g, ''))*parseInt()
+        unvPur.ratings = $('#details-vp').find(".col-md-6").find("label").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        data.unveriPurchase = unvPur;
+
+        let sus = {}
+        sus.title = "Suspicious Reviewers";
+        sus.perc = $('#details-su').find(".pct-percent").first().text();
+        sus_ratings = $('#details-su').find(".col-md-6").find("label").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+
+        sus.ratings = (sus_ratings.length >= 2) ? [sus_ratings[0], sus_ratings[1]] : [];
+        data.sus = sus;
+
+        let trend = {}
+        trend.title = "Rating Trend";
+        trend.perc = $('#details-rt').find(".pct-percent").text();
+        data.trend = trend;
+
+        let rep = {}
+        rep.title = "Phrase Repetition";
+        rep.perc = $('#details-pr').find(".pct-percent").text();
+        rep.ratings = $('#details-pr').find(".col-md-6").find("label").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        inReviewArr = $('.text_analysis_show').find("b").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        rep.inReview = [];
+        for (let i = 0; i < inReviewArr.length; i = i + 2) {
+            rep.inReview.push({ text: inReviewArr[i], perc: inReviewArr[i + 1] })
+        }
+
+        inTitleArr = $('.title_analysis_show').find("b").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        rep.inTitles = [];
+        for (let i = 0; i < inTitleArr.length; i = i + 2) {
+            rep.inTitles.push({ text: inTitleArr[i], perc: inTitleArr[i + 1] })
+        }
+
+        data.phaseRep = rep;
+
+        let wc = {}
+        wc.title = "Overrepresented Word Count";
+        wc.perc = $('#details-wc').find(".pct-percent").text();
+        data.wordCount = wc;
+
+        let lapping = {}
+        lapping.title = "Overlapping Review History";
+        lapping.perc = $('#details-rh').find(".pct-percent").text();
+        lapping.ratings = $('#details-rh').find(".col-md-6").find("label").map(function () {
+            return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
+        }).toArray();
+        data.overl = lapping;
+
+        let del = {}
+        del.title = "Deleted Reviews";
+        del.num = $('#details-dl').find(".pct-percent").text();
+        data.deleted = del;
+
+
+        const newsUrl = `https://newsapi.org/v2/everything?q=${prodName.split(' ').slice(0,3).join('%20')}&sortBy=relevancy&language=en&apiKey=4960f3e852cb41c2a30fa08ee94fe9bb`;
+        let news = await axios.request({
+            method: "GET",
+            url: newsUrl,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+            }
+        }).catch((err) => console.log(err));
+        data.news = news;
+
+
+        return data;
+
+    } catch (error) {
+        return { error };
     }
-    if (response.status !== 200) {
-        console.log("Error occurred while fetching data");
-        return;
-    }
 
-    let $ = cheerio.load(response.data);
-
-    let data = {};
-
-    data.prodName = $('#product_name').find('span').text().replace(/\t/g, '').replace(/\n/g, '');
-    data.totRev = $('fieldset').find('b').map(function () {
-        return $(this).text().trim();
-    }).toArray()[0];
-    data.adjRev = $('fieldset').find('b').map(function () {
-        return $(this).text().trim();
-    }).toArray()[1];
-    data.orgRat = $('fieldset').find('.orig-rating-dim').text();
-    data.adjRat = $('fieldset').find('#adjusted-rating-large').text();
-    data.fakeReviews = (100 - parseInt(data.adjRev.replace(/\,/g, '')) * 100 / parseInt(data.totRev.replace(/\,/g, ''))).toFixed(2) + "%";
-    data.img = $('#product_image').find('img').attr('src');
-
-
-    data.amazon = await fetchAmazon(url); //fetch amazon
-
-    let trusted = {};
-
-    trusted.rating = $('#good-reviews').find("#sample_reviews").find(".bw-rating").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    // .first().text().replace(/\t/g, '').replace(/\n/g, '');
-    trusted.review = $('#good-reviews').find(".show-actual-review").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
-    }).toArray();
-    // .first().text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
-
-    trusted.showMore = $('#good-reviews').find(".show-actual-review").find("a").map(function () {
-        return $(this).attr('href');
-    }).toArray();
-
-    let mostTrusted = {};
-    mostTrusted.title = "Most Trusted Reviews";
-    let reviews = [];
-    for (let i = 0; i < 5; i++) {
-        reviews.push({
-            rating: trusted.rating[i],
-            review: trusted.review[i],
-            showMore: trusted.showMore[i],
-        })
-    }
-    mostTrusted.reviews = reviews;
-    data.trusted = mostTrusted;
-
-    let ltrusted = {};
-    ltrusted.rating = $('#bad-reviews').find("#sample_reviews").find(".bw-rating").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    ltrusted.review = $('#bad-reviews').find(".show-actual-review").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').slice(0, -20);
-    }).toArray();
-    ltrusted.showMore = $('#bad-reviews').find(".show-actual-review").find("a").map(function () {
-        return $(this).attr('href');
-    }).toArray();
-
-    let leastTrusted = {};
-    leastTrusted.title = "Least Trusted Reviews";
-    let lreviews = [];
-    for (let i = 0; i < 5; i++) {
-        lreviews.push({
-            rating: ltrusted.rating[i],
-            review: ltrusted.review[i],
-            showMore: ltrusted.showMore[i],
-        })
-    }
-    leastTrusted.reviews = lreviews;
-    data.leastTrusted = leastTrusted;
-
-
-    let unvPur = {}
-    unvPur.title = "Unverified Purchases";
-    unvPur.perc = $('#details-vp').find(".pct-percent").text();
-    unvPur.num = $('#details-vp').find(".col-md-6").find('b').first().text();
-    // unvPur.num = parseInt(data.totRev.replace(/\,/g, ''))*parseInt()
-    unvPur.ratings = $('#details-vp').find(".col-md-6").find("label").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    data.unveriPurchase = unvPur;
-
-    let sus = {}
-    sus.title = "Suspicious Reviewers";
-    sus.perc = $('#details-su').find(".pct-percent").first().text();
-    sus_ratings = $('#details-su').find(".col-md-6").find("label").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-
-    sus.ratings = (sus_ratings.length >= 2) ? [sus_ratings[0], sus_ratings[1]] : [];
-    data.sus = sus;
-
-    let trend = {}
-    trend.title = "Rating Trend";
-    trend.perc = $('#details-rt').find(".pct-percent").text();
-    data.trend = trend;
-
-    let rep = {}
-    rep.title = "Phrase Repetition";
-    rep.perc = $('#details-pr').find(".pct-percent").text();
-    rep.ratings = $('#details-pr').find(".col-md-6").find("label").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    inReviewArr = $('.text_analysis_show').find("b").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    rep.inReview = [];
-    for (let i = 0; i < inReviewArr.length; i = i + 2) {
-        rep.inReview.push({ text: inReviewArr[i], perc: inReviewArr[i + 1] })
-    }
-
-    inTitleArr = $('.title_analysis_show').find("b").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    rep.inTitles = [];
-    for (let i = 0; i < inTitleArr.length; i = i + 2) {
-        rep.inTitles.push({ text: inTitleArr[i], perc: inTitleArr[i + 1] })
-    }
-
-    data.phaseRep = rep;
-
-    let wc = {}
-    wc.title = "Overrepresented Word Count";
-    wc.perc = $('#details-wc').find(".pct-percent").text();
-    data.wordCount = wc;
-
-    let lapping = {}
-    lapping.title = "Overlapping Review History";
-    lapping.perc = $('#details-rh').find(".pct-percent").text();
-    lapping.ratings = $('#details-rh').find(".col-md-6").find("label").map(function () {
-        return $(this).text().replace(/\t/g, '').replace(/\n/g, '').trim();
-    }).toArray();
-    data.overl = lapping;
-
-    let del = {}
-    del.title = "Deleted Reviews";
-    del.num = $('#details-dl').find(".pct-percent").text();
-    data.deleted = del;
-
-    return data;
 }
 
 // fetchData(url).then((html) => {
